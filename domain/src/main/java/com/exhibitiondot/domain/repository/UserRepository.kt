e34796774c2a2ca6b9d335888c7119da57526010
안승우 <rio319@naver.com>
2024-10-15 23:30:05 +0900

    #1 feat: UserRepository 선언

---
+++ domain/src/main/java/com/exhibitiondot/domain/repository/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.exhibitiondot.domain.repository
+
+import com.exhibitiondot.domain.model.User
+
+interface UserRepository {
+    suspend fun signIn(email: String): Result<Long>
+
+    suspend fun signUp(user: User): Result<Unit>
+
+    suspend fun changeUserInfo(user: User): Result<Unit>
+}
