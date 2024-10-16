b44010f234032aa312ab79917e4700933067122e
안승우 <rio319@naver.com>
2024-10-16 13:48:27 +0900

    #5 setting: Retrofit2 세팅

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -41,4 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // retrofit
+    implementation(libs.retrofit)
 }
