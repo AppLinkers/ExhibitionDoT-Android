02e16c2b98091e1057152662f76cf51bebc8bea3
안승우 <rio319@naver.com>
2024-10-16 14:07:22 +0900

    #5 setting: Kotlinx-Serialization 세팅

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -35,4 +36,7 @@ dependencies {
 
     // retrofit
     implementation(libs.retrofit)
+
+    // kotlinx-serialization
+    implementation(libs.kotlinx.serialization)
 }
