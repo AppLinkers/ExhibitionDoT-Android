13a75211008d85dbfd9b67d876650adcf109230f
안승우 <rio319@naver.com>
2024-10-29 18:17:32 +0900

    #17 feat: MainGraph Navigation 작성

---
+++ presentation/src/main/java/com/exhibitiondot/presentation/ui/screen/main/navigation/MainNavigation.kt
@@ -0,0 +1,45 @@
+package com.exhibitiondot.presentation.ui.screen.main.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import androidx.navigation.navigation
+import com.exhibitiondot.presentation.ui.DoTAppState
+import com.exhibitiondot.presentation.ui.navigation.KEY_EVENT_ID
+import com.exhibitiondot.presentation.ui.navigation.Screen
+import com.exhibitiondot.presentation.ui.navigation.ScreenGraph
+
+fun NavController.navigateToMainGraph() = navigate(Screen.Home.route) {
+    popUpTo(ScreenGraph.MainGraph.route) { inclusive = true }
+}
+
+fun NavController.navigateToEventDetail(eventId: Long) =
+    navigate("${Screen.EventDetail.route}/$eventId")
+
+fun NavGraphBuilder.nestedMainGraph(appState: DoTAppState) {
+    navigation(
+        startDestination = Screen.Home.route,
+        route = ScreenGraph.SignGraph.route
+    ) {
+        composable(
+            route = Screen.Home.route
+        ) {
+
+        }
+        composable(
+            route = "${Screen.EventDetail.route}/{$KEY_EVENT_ID}",
+            arguments = listOf(
+                navArgument(KEY_EVENT_ID) { type = NavType.LongType }
+            )
+        ) {
+
+        }
+        composable(
+            route = Screen.My.route
+        ) {
+
+        }
+    }
+}
