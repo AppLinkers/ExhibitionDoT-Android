9c294cfea0589206a60a9d130e267fc0fe339b07
안승우 <rio319@naver.com>
2024-10-31 13:33:29 +0900

    #17 feat: Event, Comment UiModel & Mapper 구현

---
+++ presentation/src/main/java/com/exhibitiondot/presentation/mapper/CommentMapper.kt
@@ -0,0 +1,12 @@
+package com.exhibitiondot.presentation.mapper
+
+import com.exhibitiondot.domain.model.Comment
+import com.exhibitiondot.presentation.model.CommentUiModel
+
+fun Comment.toUiModel() =
+    CommentUiModel(
+        id = id,
+        nickname = authorNickname,
+        content = content,
+        createdAt = createdAt.format(DateFormatStrategy.RelativeTime)
+    )
--- presentation/src/main/java/com/exhibitiondot/presentation/mapper/DateMapper.kt
+++ presentation/src/main/java/com/exhibitiondot/presentation/mapper/DateMapper.kt
@@ -1,12 +1,16 @@
 package com.exhibitiondot.presentation.mapper
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 sealed interface DateFormatStrategy {
     fun format(date: String): String
 
     data object FullDate : DateFormatStrategy {
         override fun format(date: String): String {
             val (yyyy, mm, dd) = date.split(SPLIT_DELIMITER)
-            return "${yyyy}년 ${mm}월 ${dd}일"
+            return "${yyyy}.${mm}.${dd}일"
         }
     }
 
@@ -17,8 +21,30 @@ sealed interface DateFormatStrategy {
         }
     }
 
+    data object RelativeTime : DateFormatStrategy {
+        override fun format(date: String): String {
+            val parsedDate = formatter.parse(date)
+            val now = Date()
+            val diff = now.time - (parsedDate as Date).time
+
+            val days = diff / (24 * 60 * 60 * 1_000)
+            val months = days / 30
+            val years = months / 12
+            return when {
+                days == 0L -> "오늘"
+                days <= 30 -> "${days}일 전"
+                months < 12 -> "${months}달 전"
+                else -> "${years}년 전"
+            }
+        }
+    }
+
     companion object {
         private const val SPLIT_DELIMITER = "-"
+        private const val DATE_PATTERN = "yyyy-MM-dd"
+
+        private val formatter = SimpleDateFormat(DATE_PATTERN, Locale.KOREA)
+
     }
 }
 
