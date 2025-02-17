4588294a3fce1d2e5bca2da09e7475fee7211560
안승우 <rio319@naver.com>
2025-02-17 16:50:06 +0900

    #29 fix: 기기 navigation bar의 padding 적용

--- presentation/src/main/java/com/exhibitiondot/presentation/ui/DoTApp.kt
+++ presentation/src/main/java/com/exhibitiondot/presentation/ui/DoTApp.kt
@@ -3,30 +3,25 @@ package com.exhibitiondot.presentation.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.exhibitiondot.presentation.ui.navigation.DoTNavHost
 
 @Composable
 fun DoTApp(
     appState: DoTAppState,
 ) {
-    Scaffold(
-        containerColor = MaterialTheme.colorScheme.surface,
-        contentWindowInsets = WindowInsets(0.dp)
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .consumeWindowInsets(padding)
-                .background(color = MaterialTheme.colorScheme.background)
-        ) {
-            DoTNavHost(appState = appState)
-        }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background)
+            .padding(bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding())
+    ) {
+        DoTNavHost(appState = appState)
     }
 }
