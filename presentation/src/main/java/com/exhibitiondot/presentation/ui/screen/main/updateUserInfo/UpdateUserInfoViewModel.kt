ccde17ae1abd422f898a5b5093923b247be10699
안승우 <rio319@naver.com>
2025-02-17 10:59:05 +0900

    #27 feat: 유저 정보 수정 ViewModel 구현

---
+++ presentation/src/main/java/com/exhibitiondot/presentation/ui/screen/main/updateUserInfo/UpdateUserInfoViewModel.kt
@@ -0,0 +1,71 @@
+package com.exhibitiondot.presentation.ui.screen.main.updateUserInfo
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import com.exhibitiondot.domain.model.Category
+import com.exhibitiondot.domain.model.EventType
+import com.exhibitiondot.domain.model.Region
+import com.exhibitiondot.domain.model.UpdateUserInfo
+import com.exhibitiondot.domain.usecase.user.GetCacheFirstUserFlowUseCase
+import com.exhibitiondot.domain.usecase.user.UpdateUserInfoUseCase
+import com.exhibitiondot.presentation.base.BaseViewModel
+import com.exhibitiondot.presentation.model.GlobalUiModel
+import com.exhibitiondot.presentation.ui.state.EditTextState
+import com.exhibitiondot.presentation.ui.state.MultiFilterState
+import com.exhibitiondot.presentation.ui.state.SingleFilterState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UpdateUserInfoViewModel @Inject constructor(
+    private val getCacheFirstUserFlowUseCase: GetCacheFirstUserFlowUseCase,
+    private val updateUserInfoUseCase: UpdateUserInfoUseCase,
+    private val uiModel: GlobalUiModel,
+) : BaseViewModel() {
+    var uiState by mutableStateOf<UpdateUserInfoUiState>(UpdateUserInfoUiState.PostLoading)
+        private set
+
+    val nicknameState = EditTextState(maxLength = 10)
+    val regionState = SingleFilterState(filterList = Region.values())
+    val categoryState = MultiFilterState(filterList = Category.values())
+    val eventTypeState = MultiFilterState(filterList = EventType.values())
+
+    init {
+        viewModelScope.launch {
+            val user = getCacheFirstUserFlowUseCase().first()
+            with(user) {
+                nicknameState.typeText(nickname)
+                regionState.setFilter(region)
+                categoryState.setFilter(categoryList)
+                eventTypeState.setFilter(eventTypeList)
+            }.also {
+                uiState = UpdateUserInfoUiState.Idle
+            }
+        }
+    }
+
+    fun onUpdate(onBack: () -> Unit) {
+        uiState = UpdateUserInfoUiState.Loading
+        viewModelScope.launch {
+            val updateUserInfo = UpdateUserInfo(
+                nickname = nicknameState.trimmedText(),
+                region = regionState.selectedFilter!!,
+                categoryList = categoryState.selectedFilterList,
+                eventTypeList = eventTypeState.selectedFilterList
+            )
+            updateUserInfoUseCase(updateUserInfo)
+                .onSuccess {
+                    uiModel.showToast("정보를 수정했어요")
+                    onBack()
+                }
+                .onFailure {
+                    uiModel.showToast("정보 수정에 실패했어요")
+                    uiState = UpdateUserInfoUiState.Idle
+                }
+        }
+    }
+}
