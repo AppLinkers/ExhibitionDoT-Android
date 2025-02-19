fb8d8c9e248c908ee94b45a68690b11cfe2f24de
안승우 <rio319@naver.com>
2025-02-19 13:49:28 +0900

    #31 feat: MyPage 정보 구분줄 추가

--- presentation/src/main/java/com/exhibitiondot/presentation/ui/screen/sign/signUp/SignUpStep.kt
+++ presentation/src/main/java/com/exhibitiondot/presentation/ui/screen/sign/signUp/SignUpStep.kt
@@ -3,34 +3,6 @@ package com.exhibitiondot.presentation.ui.screen.sign.signUp
 import androidx.annotation.StringRes
 import com.exhibitiondot.presentation.R
 
-/*sealed class SignUpStep(
-    @StringRes val description: Int,
-    val percentage: Float,
-) {
-    abstract fun prevStep() : SignUpStep?
-    abstract fun nextStep() : SignUpStep?
-
-    data object InfoStep : SignUpStep(R.string.signup_info_step_description, 0.1f) {
-        override fun prevStep(): SignUpStep? = null
-        override fun nextStep(): SignUpStep = RegionStep
-    }
-
-    data object RegionStep : SignUpStep(R.string.signup_region_step_description, 0.4f) {
-        override fun prevStep(): SignUpStep = InfoStep
-        override fun nextStep(): SignUpStep = CategoryStep
-    }
-
-    data object CategoryStep : SignUpStep(R.string.signup_category_step_description, 0.7f) {
-        override fun prevStep(): SignUpStep = RegionStep
-        override fun nextStep(): SignUpStep = EventTypeStep
-    }
-
-    data object EventTypeStep : SignUpStep(R.string.signup_event_type_step_description, 1.0f) {
-        override fun prevStep(): SignUpStep = CategoryStep
-        override fun nextStep(): SignUpStep? = null
-    }
-}*/
-
 enum class SignUpStep(
     @StringRes val description: Int,
     val percentage: Float,
